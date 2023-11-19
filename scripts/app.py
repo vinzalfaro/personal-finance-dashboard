d65f605bb6d7e1205649511cef7ec8fb03aac2d8
Vinz <120233304+entropy143@users.noreply.github.com>
2023-11-19 20:29:09 +0800

    Write accounts_over_time query and visualize chart using plotly and streamlit[200~

--- scripts/app.py
+++ scripts/app.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from database import etl
 from read_queries import query
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(page_title='My Personal Finance App',
                    page_icon=':money_with_wings:',
@@ -10,12 +11,27 @@ st.set_page_config(page_title='My Personal Finance App',
 
 st.title('My Personal Finance App')
 
-tab1, tab2, tab3, tab4 = st.tabs(['Cash Flow', 'Account Balance Over Time', 'Expenses Per Category', 'Insights'])
+tab1, tab2, tab3, tab4 = st.tabs(['Net Worth', 'Accounts', 'Expenses Per Category', 'Insights'])
 
 connection_uri = "postgresql+psycopg2://postgres:password@localhost:5432/personal_finance_dashboard"
 etl(connection_uri)
 net_worth_over_time = query("net_worth_over_time")
+accounts_over_time = query("accounts_over_time")
+amount_over_time = pd.merge(net_worth_over_time, accounts_over_time, on='date')
+print(amount_over_time.head(20))
 
-print(net_worth_over_time.head(20))
 
-#st.line_chart(net_worth_over_time, x = "date", y = "net_worth")
+with tab1:
+    st.header("Net Worth")
+
+    column_options = ['binance', 'gcash', 'grabpay', 'maya', 'ronin', 'seabank', 'shopeepay', 'unionbank', 'wallet', 'net_worth']
+    selected_columns = st.multiselect('Select accounts to display:', column_options, default='net_worth')
+
+    # create a line chart using the selected columns
+    fig_account_balance = px.line(amount_over_time , x='date', y=selected_columns, title='Account Balance Over Time', 
+    width=1200, height= 600)
+
+    # display the chart
+    st.plotly_chart(fig_account_balance)
+with tab2:
+    st.header("Accounts")
--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,16 +1,25 @@
 --@name: transactions
-SELECT * FROM transactions
+SELECT * FROM transactions;
 
 --@name: net_worth_over_time
 SELECT
     date,
-    SUM(net_amount) OVER (ORDER BY date) AS net_worth
-FROM (
-    SELECT
-        date,
-        SUM(amount) AS net_amount
-    FROM
-        transactions
-    GROUP BY
-        date);
+    SUM(amount) OVER (ORDER BY date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS net_worth
+FROM transactions;
 
+--@name: accounts_over_time
+SELECT
+    date,
+    SUM(CASE WHEN Account = 'Wallet' THEN amount ELSE 0 END) OVER (ORDER BY date) AS wallet,
+    SUM(CASE WHEN Account = 'Union Bank' THEN amount ELSE 0 END) OVER (ORDER BY date) AS unionbank,
+    SUM(CASE WHEN Account = 'Seabank' THEN amount ELSE 0 END) OVER (ORDER BY date) AS seabank,
+    SUM(CASE WHEN Account = 'GCash' THEN amount ELSE 0 END) OVER (ORDER BY date) AS gcash,
+    SUM(CASE WHEN Account = 'Maya' THEN amount ELSE 0 END) OVER (ORDER BY date) AS maya,
+    SUM(CASE WHEN Account = 'GrabPay' THEN amount ELSE 0 END) OVER (ORDER BY date) AS grabpay,
+    SUM(CASE WHEN Account = 'ShopeePay' THEN amount ELSE 0 END) OVER (ORDER BY date) AS shopeepay,
+    SUM(CASE WHEN Account = 'Binance' THEN amount ELSE 0 END) OVER (ORDER BY date) AS binance,
+    SUM(CASE WHEN Account = 'Ronin' THEN amount ELSE 0 END) OVER (ORDER BY date) AS ronin
+FROM
+    transactions
+ORDER BY
+    date;
