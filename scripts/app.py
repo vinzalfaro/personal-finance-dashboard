0eca96153f4873086447a4ae473a35e1716fe301
Vinz <120233304+entropy143@users.noreply.github.com>
2023-11-28 12:21:36 +0800

    Improve dashboard layout

--- scripts/app.py
+++ scripts/app.py
@@ -13,67 +13,75 @@ st.set_page_config(page_title='My Personal Finance App',
                    layout='wide')
 
 st.title('My Personal Finance App')
-tab1, tab2, tab3 = st.tabs(['Accounts', 'Expenses', 'Income'])
+tab1, tab2, tab3 = st.tabs(['Home', 'Dashboard', 'Data'])
+
+sidebar_main = st.sidebar.selectbox('Navigation', ['Home','Dashboard', 'Explore'])
+view = st.sidebar.radio("Select view:", ["monthly", "weekly", "daily"], horizontal = True, key = "sidebar")
 
 with tab1:
-    st.header("Accounts")
-    amount_over_time = query("amount_over_time")
-    column_options = ['binance', 'gcash', 'grabpay', 'maya', 'ronin', 'seabank', 'shopeepay', 'unionbank', 'wallet', 'net_worth']
-    selected_columns = st.multiselect('Select accounts to display:', column_options, default='net_worth')
-    fig_accounts_over_time = px.line(amount_over_time , x='date', y=selected_columns, title='Account Balance Over Time', 
-    width=1200, height= 600)
-    st.plotly_chart(fig_accounts_over_time)
-
-    st.header("Payment Methods")
-    payment_methods = query("payment_methods")
-    fig_payment_methods = px.bar(payment_methods, x='amount', y='account', title='Payment Methods')
-    st.plotly_chart(fig_payment_methods)
-
-    st.header("Receiving Methods")
-    receiving_methods = query("receiving_methods")
-    fig_receiving_methods = px.bar(receiving_methods, x='amount', y='account', title='Receiving Methods')
-    st.plotly_chart(fig_receiving_methods)
-
+    sidebar = 'Home'
 
 with tab2:
-    st.header("Expenditure")
-    monthly_expenses = query("monthly_expenses")
-    fig_monthly_expenses = px.bar(monthly_expenses, x='month', y='expenses', title='Monthly Expenses')
-    st.plotly_chart(fig_monthly_expenses)
+    sidebar = 'Dashboard'
+    with st.container():
+        column_options = ['binance', 'gcash', 'grabpay', 'maya', 'ronin', 'seabank', 'shopeepay', 'unionbank', 'wallet', 'net_worth']
+        selected_columns = st.multiselect('Select accounts to display:', column_options, default='net_worth')
+        if view == 'monthly':   
+            monthly_amount_over_time = query("monthly_amount_over_time")
+            fig_accounts_over_time = px.line(monthly_amount_over_time , x='month', y=selected_columns, title='Account Balance Over Time')
+            st.plotly_chart(fig_accounts_over_time, use_container_width= True)
 
-    weekly_expenses = query("weekly_expenses")
-    fig_weekly_expenses = px.bar(weekly_expenses, x='week', y='expenses', title='Weekly Expenses')
-    st.plotly_chart(fig_weekly_expenses)
+        elif view == 'weekly':   
+            weekly_amount_over_time = query("weekly_amount_over_time")
+            fig_accounts_over_time = px.line(weekly_amount_over_time , x='week', y=selected_columns, title='Account Balance Over Time')
+            st.plotly_chart(fig_accounts_over_time, use_container_width= True)
 
-    daily_expenses = query("daily_expenses")
-    fig_daily_expenses = px.bar(daily_expenses, x='day', y='expenses', title='Daily Expenses')
-    st.plotly_chart(fig_daily_expenses)
+        elif view == 'daily':   
+            daily_amount_over_time = query("daily_amount_over_time")
+            fig_accounts_over_time = px.line(daily_amount_over_time , x='day', y=selected_columns, title='Account Balance Over Time')
+            st.plotly_chart(fig_accounts_over_time, use_container_width= True)
+        
+    b1, b2 = st.columns(2)
+    with b1:
+        payment_methods = query("payment_methods")
+        fig_payment_methods = px.bar(payment_methods, x='account', y='amount', title='Payment Methods')
+        st.plotly_chart(fig_payment_methods, use_container_width= True)
+    with b2:
+        receiving_methods = query("receiving_methods")
+        fig_receiving_methods = px.bar(receiving_methods, x='account', y='amount', title='Receiving Methods')
+        st.plotly_chart(fig_receiving_methods, use_container_width= True)
 
+    c1, c2 = st.columns(2)
+    with c1:
+        expenses_per_category = query("expenses_per_category")
+        fig_expenses_by_category = px.pie(expenses_per_category, values='expenses', title='Expenses per category', names='category', hole=0.4)
+        fig_expenses_by_category.update_traces(textposition='inside', textinfo='percent+label')
+        st.plotly_chart(fig_expenses_by_category, use_container_width= True)
+        with st.expander("See top expenses"):
+            st.header("Top expenses")
+            st.dataframe(expenses_per_category, height=550, use_container_width= True)
+    with c2:
+        income_per_category = query("income_per_category")
+        fig_income = px.pie(income_per_category, values='income', names='category', title='Income per category', hole=0.4)
+        fig_income.update_traces(textposition='inside', textinfo='percent+label')
+        st.plotly_chart(fig_income, use_container_width= True)
+        with st.expander("See top income sources"):
+            st.header("Top income sources")
+            st.dataframe(income_per_category, height=550, use_container_width= True)
 
-    st.header("Expenses per category")
-    expenses_per_category = query("expenses_per_category")
-    fig_expenses_by_category = px.pie(expenses_per_category, values='expenses', names='category', width=700, height=700, hole=0.4,)
-    fig_expenses_by_category.update_traces(textposition='inside', textinfo='percent+label')
-    st.plotly_chart(fig_expenses_by_category)
-
-    st.header("Top expenses")
-    st.dataframe(expenses_per_category, height=550, use_container_width= True)
-    
-
-    
+    with st.container():
+        if view == 'monthly':
+            monthly_expenses = query("monthly_expenses")
+            fig_monthly_expenses = px.line(monthly_expenses, x='month', y='expenses', title='Monthly Expenses')
+            st.plotly_chart(fig_monthly_expenses, use_container_width= True)
+        elif view == "weekly":
+            weekly_expenses = query("weekly_expenses")
+            fig_weekly_expenses = px.line(weekly_expenses, x='week', y='expenses', title='Weekly Expenses')
+            st.plotly_chart(fig_weekly_expenses, use_container_width= True)
+        elif view == "daily":
+            daily_expenses = query("daily_expenses")
+            fig_daily_expenses = px.line(daily_expenses, x='day', y='expenses', title='Daily Expenses')
+            st.plotly_chart(fig_daily_expenses, use_container_width= True)
 
 with tab3:
-    st.header("Income")
-
-    income_per_category = query("income_per_category")
-    
-    fig_income = px.pie(income_per_category, values='income', names='category', width=700, height=700, hole=0.4,)
-    fig_income.update_traces(textposition='inside', textinfo='percent+label')
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.plotly_chart(fig_income)
-
-    with col2:
-        st.dataframe(income_per_category, height=550, use_container_width= True)
+    sidebar = 'Data'
