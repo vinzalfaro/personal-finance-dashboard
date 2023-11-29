1d0f0b2dfc543081a62666596d55e4c424513fb2
Vinz <120233304+entropy143@users.noreply.github.com>
2023-11-29 10:09:51 +0800

    Display raw and cleaned transactions dataframe on data tab

--- scripts/app.py
+++ scripts/app.py
@@ -1,6 +1,4 @@
-import pandas as pd
-from sqlalchemy import create_engine
-from database import etl
+from database import etl, extract
 from read_queries import query
 import streamlit as st
 import plotly.express as px
@@ -9,23 +7,42 @@ st.set_page_config(page_title='My Personal Finance App',
                    page_icon=':money_with_wings:',
                    layout='wide')
 
-st.title('My Personal Finance App')
-tab1, tab2, tab3 = st.tabs(['Home', 'Dashboard', 'Data'])
+st.title('Personal Finance App')
+tab1, tab2, tab3 = st.tabs(['Home', 'Data', 'Dashboard'])
 
-sidebar_main = st.sidebar.selectbox('Navigation', ['Home','Dashboard', 'Explore'])
-view = st.sidebar.radio("Select view:", ["monthly", "weekly", "daily"], horizontal = True, key = "sidebar")
+with st.sidebar:
+    st.header('Filters')
+    column_options = ['binance', 'gcash', 'grabpay', 'maya', 'ronin', 'seabank', 'shopeepay', 'unionbank', 'wallet', 'net_worth']
+    selected_columns = st.multiselect('Select accounts to display:', column_options, default='net_worth')
+    view = st.radio("Select view:", ["monthly", "weekly", "daily"], horizontal = True, key = "sidebar")
 
 with tab1:
-    transactions_list = st.file_uploader("Upload transactions_list.csv here")
-    if transactions_list is not None:
-        connection_uri = "postgresql+psycopg2://postgres:password@localhost:5432/personal_finance_dashboard"
-        etl(transactions_list, connection_uri)
+    with st.container():
+        st.markdown("""
+                    Hi! Welcome to Personal Finance App. You can find the app's GitHub repository [here](https://github.com/vinzalfaro/bluecoins_dashboard).
+                    Personal Finance App creates an informative dashboard from your Bluecoins transaction data. 
+                    Bluecoins is an expense tracking app which allows exporting of your data in csv format. 
+                    Please upload your transactions csv file below and go to the "Dashboard" tab to see your analytics dashboard. 
+                    """ )
+
+        transactions_list = st.file_uploader("Upload file here")
+        if st.button('Create Dashboard'):
+            if transactions_list is not None:
+                connection_uri = "postgresql+psycopg2://postgres:password@localhost:5432/personal_finance_dashboard"
+                etl(transactions_list, connection_uri)
 
 with tab2:
-    sidebar = 'Dashboard'
     with st.container():
-        column_options = ['binance', 'gcash', 'grabpay', 'maya', 'ronin', 'seabank', 'shopeepay', 'unionbank', 'wallet', 'net_worth']
-        selected_columns = st.multiselect('Select accounts to display:', column_options, default='net_worth')
+        raw_transactions = extract(transactions_list)
+        st.markdown("Raw Transactions Dataframe")
+        st.dataframe(raw_transactions, height=400, use_container_width= True)
+    with st.container():
+        cleaned_transactions = query("transactions")
+        st.markdown("Cleaned Transactions Dataframe")
+        st.dataframe(cleaned_transactions, height=400, use_container_width= True)
+
+with tab3:
+    with st.container():
         if view == 'monthly':   
             monthly_amount_over_time = query("monthly_amount_over_time")
             fig_accounts_over_time = px.line(monthly_amount_over_time , x='month', y=selected_columns, title='Account Balance Over Time')
@@ -40,7 +57,9 @@ with tab2:
             daily_amount_over_time = query("daily_amount_over_time")
             fig_accounts_over_time = px.line(daily_amount_over_time , x='day', y=selected_columns, title='Account Balance Over Time')
             st.plotly_chart(fig_accounts_over_time, use_container_width= True)
-        
+
+    st.markdown("""---""")
+
     b1, b2 = st.columns(2)
     with b1:
         payment_methods = query("payment_methods")
@@ -51,6 +70,8 @@ with tab2:
         fig_receiving_methods = px.bar(receiving_methods, x='account', y='amount', title='Receiving Methods')
         st.plotly_chart(fig_receiving_methods, use_container_width= True)
 
+    st.markdown("""---""")
+
     c1, c2 = st.columns(2)
     with c1:
         expenses_per_category = query("expenses_per_category")
@@ -59,7 +80,7 @@ with tab2:
         st.plotly_chart(fig_expenses_by_category, use_container_width= True)
         with st.expander("See top expenses"):
             st.header("Top expenses")
-            st.dataframe(expenses_per_category, height=550, use_container_width= True)
+            st.dataframe(expenses_per_category, height=400, use_container_width= True)
     with c2:
         income_per_category = query("income_per_category")
         fig_income = px.pie(income_per_category, values='income', names='category', title='Income per category', hole=0.4)
@@ -67,7 +88,9 @@ with tab2:
         st.plotly_chart(fig_income, use_container_width= True)
         with st.expander("See top income sources"):
             st.header("Top income sources")
-            st.dataframe(income_per_category, height=550, use_container_width= True)
+            st.dataframe(income_per_category, height=400, use_container_width= True)
+
+    st.markdown("""---""")
 
     with st.container():
         if view == 'monthly':
@@ -82,6 +105,3 @@ with tab2:
             daily_expenses = query("daily_expenses")
             fig_daily_expenses = px.line(daily_expenses, x='day', y='expenses', title='Daily Expenses')
             st.plotly_chart(fig_daily_expenses, use_container_width= True)
-
-with tab3:
-    sidebar = 'Data'
