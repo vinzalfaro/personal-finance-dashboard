73fb0f5ca317c24a11aa0dce6d9db9bc77c2f473
vinzgdec <va.alfaro@greatdealscorp.com>
2024-04-06 19:17:43 +0800

    Edit connection detailes

--- scripts/read_queries.py
+++ scripts/read_queries.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def read_query(query_name):
-    with open("queries.sql", 'r') as file:
+    with open("scripts/queries.sql", 'r') as file:
         content = file.read()
 
     queries = [q.strip() for q in content.split('--@name:')]
@@ -16,7 +16,7 @@ def read_query(query_name):
     raise ValueError(f"Query with name '{query_name}' not found in the file.")
 
 def query(query_name):
-    connection_uri = "postgresql+psycopg2://postgres:password@localhost:5432/personal_finance_dashboard"
+    connection_uri = "postgresql://postgres:password@postgres:5432/personal_finance_dashboard"
     query = read_query(query_name)
     db_engine = create_engine(connection_uri)
     df = pd.read_sql(query, db_engine)
